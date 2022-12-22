e9650cbdfc4bf12a7bd91f81593811a16dd14e8a
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-12-22 09:53:23 +0000

    #9 related

--- arenets/arekit/contrib/utils/data/storages/pandas_based.py
+++ arenets/arekit/contrib/utils/data/storages/pandas_based.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 from arenets.arekit.common.data.storages.base import BaseRowsStorage
@@ -13,27 +12,10 @@ class PandasBasedRowsStorage(BaseRowsStorage):
         self._df = df
 
     @staticmethod
-    def __create_empty(cols_with_types):
-        """ cols_with_types: list of pairs ("name", dtype)
-        """
-        assert(isinstance(cols_with_types, list))
-        data = np.empty(0, dtype=np.dtype(cols_with_types))
-        return pd.DataFrame(data)
-
-    def __filter(self, column_name, value):
-        return self._df[self._df[column_name] == value]
-
-    @staticmethod
     def __iter_rows_core(df):
         assert(isinstance(df, pd.DataFrame))
         for row_index, row in df.iterrows():
             yield row_index, row
-
-    def __fill_with_blank_rows(self, row_id_column_name, rows_count):
-        assert(isinstance(row_id_column_name, str))
-        assert(isinstance(rows_count, int))
-        self._df[row_id_column_name] = list(range(rows_count))
-        self._df.set_index(row_id_column_name, inplace=True)
 
     # region protected methods
 
