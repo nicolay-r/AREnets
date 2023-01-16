ea0d61a217748a66710e02720f2ae06ea5ac1a42
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-01-16 11:16:26 +0000

    #57 removed column_extra_funcs

--- arenets/core/predict/provider/base.py
+++ arenets/core/predict/provider/base.py
@@ -1,4 +1,4 @@
 class BasePredictProvider(object):
 
-    def provide(self, sample_id_with_uint_labels_iter, labels_count, column_extra_funcs=None):
+    def provide(self, sample_id_with_uint_labels_iter, labels_count):
         raise NotImplementedError()
--- arenets/core/predict/provider/id_and_binary_labels.py
+++ arenets/core/predict/provider/id_and_binary_labels.py
@@ -12,7 +12,7 @@ class IdAndBinaryLabelsPredictProvider(BasePredictProvider):
     """
 
     @staticmethod
-    def __iter_contents(sample_id_with_uint_labels_iter, labels_count, column_extra_funcs):
+    def __iter_contents(sample_id_with_uint_labels_iter, labels_count):
         assert(isinstance(labels_count, int))
 
         for sample_id, uint_label in sample_id_with_uint_labels_iter:
@@ -20,11 +20,6 @@ class IdAndBinaryLabelsPredictProvider(BasePredictProvider):
 
             # Composing row contents.
             contents = [sample_id]
-
-            # Optionally provide additional values.
-            if column_extra_funcs is not None:
-                for _, value_func in column_extra_funcs:
-                    contents.append(str(value_func(sample_id)))
 
             # Composing labels.
             labels = ['0'] * labels_count
@@ -34,21 +29,16 @@ class IdAndBinaryLabelsPredictProvider(BasePredictProvider):
             contents.extend(labels)
             yield contents
 
-    def provide(self, sample_id_with_uint_labels_iter, labels_count, column_extra_funcs=None):
+    def provide(self, sample_id_with_uint_labels_iter, labels_count):
         assert(isinstance(sample_id_with_uint_labels_iter, collections.Iterable))
-        assert(isinstance(column_extra_funcs, list) or column_extra_funcs is None)
         assert(isinstance(labels_count, int))
 
         # Provide contents.
-        contents_it = self.__iter_contents(
-            sample_id_with_uint_labels_iter=sample_id_with_uint_labels_iter,
-            labels_count=labels_count,
-            column_extra_funcs=column_extra_funcs)
+        contents_it = self.__iter_contents(sample_id_with_uint_labels_iter=sample_id_with_uint_labels_iter,
+                                           labels_count=labels_count)
 
         # Provide title.
         title = [const.ID]
-        if column_extra_funcs is not None:
-            title.extend([column_name for column_name, _ in column_extra_funcs])
         title.extend([str(label) for label in range(labels_count)])
 
         return title, contents_it
